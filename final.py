f5056410352cf8ce16169ecdd8d26b0bfe293ed2
Ram Binay Sharma <39820677+rambinay@users.noreply.github.com>
2019-11-24 18:49:30 +0545

    Add files via upload

--- final.py
+++ final.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on 7076
+Created on 2076-2-5
 
 @author: ram binay
 """
@@ -14,15 +14,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    t = requests.get('https://api.thingspeak.com/channels/780244/fields/1/last.txt')
-    tempr = float(t.text)
-    hb = requests.get('https://api.thingspeak.com/channels/780244/fields/2/last.txt')
-    fire = int(hb.text)
     l = requests.get('https://api.thingspeak.com/channels/780244/fields/1/last.txt')
-    lpg = int(l.text)
-    h = requests.get('https://api.thingspeak.com/channels/780244/fields/1/last.txt')
-    humr = float(h.text)
-    return render_template('major.html', tempr=tempr, fire=fire, lpg=lpg, humr=humr)
+    lpg = float(l.text)
+    hb = requests.get('https://api.thingspeak.com/channels/780244/fields/2/last.txt')
+    fire = float(hb.text)
+
+    alc = requests.get('https://api.thingspeak.com/channels/780244/fields/3/last.txt')
+    alcohol = float(alc.text)
+
+    return render_template('major.html', lpg=lpg, fire=fire,alcohol=alcohol)
 
 if __name__ == '__main__':
     app.run(debug=True)
